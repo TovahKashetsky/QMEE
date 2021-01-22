951ea9d1f50f918b798246eae629e2f842dd901c
TovahKashetsky <faith@Tovahs-Mac.local>
2021-01-22 17:09:01 -0400

    Adding to QMEE

--- antstatV1.R
+++ antstatV1.R
@@ -7,3 +7,10 @@ print(ant_train)
 ## must remove NAs with na.rm=TRUE ##
 tapply(ant_train$med_transport, INDEX = ant_train$group, FUN = mean, na.rm=TRUE)
 ## try a t-test ##
+cm <- 5705.552; sdc <- 1 # choice mean
+ncm <- 5434.131; sdnc <- 1 # no choice mean
+cn <- 23 # choice sample size
+ncn <- 22 # no choice sample size
+d1 <- rnorm(cn,cm,sdc)
+d2 <- rnorm(ncn,ncm,sdnc)
+t.test(d2,d1,paired=F,alternative="two.sided")
