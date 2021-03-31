fdbd713f7c832e2aa17b88a0f4b628ca4c3e966f
TovahKashetsky <faith@Tovahs-Mac.local>
2021-03-30 20:04:48 -0400

    Assignment 7 updated

--- A7.R
+++ A7.R
@@ -24,17 +24,18 @@ print(train.data) # NoChoice group should have NAs for prop_dark
 ## JD: Binomial wants successes and failures (0/1 outcomes), not proportions.
 ## I also get an error from R saying exactly that.
 ## Please advise
-
+# TK: I changed prediction 1 to quasipossion 
 train.choice <- train.data %>% group_by(group) %>% filter(group=="Choice") # make data only for choice group
 print(train.choice)
 pd <- ggplot(train.choice, aes(training,prop_dark))+geom_point()
 pd1 <- pd + geom_smooth(method="glm",colour="orange",
                            formula=y~x,
-                           method.args=list(family="binomial")) 
-pdmodel <- glm(prop_dark~training, family=binomial, data=train.choice)
+                           method.args=list(family=quasipoisson(link="log"))) 
+pdmodel <- glm(data=train.choice, prop_dark~training, family=quasipoisson(link="log"))
 summary(pdmodel)
+par(mar=c(1,1,1,1)) # margins too large
 plot(pdmodel) 
-# all of the diagnostic plots look good, possibly an outlier (5) that's right on Cooks distance
+# all of the diagnostic plots look good, I don't see any major issues. The normal q-q could be straighter ?
 acf(residuals(pdmodel)) 
 # see a slight pattern of residuals starting in positives then going negative
 
@@ -43,11 +44,12 @@ acf(residuals(pdmodel))
 mt <- ggplot(train.data, aes(training,med_transport))+geom_point()
 mt1 <- mt + geom_smooth(method="glm",colour="orange",
                         formula=y~x,
-                        method.args=list(family=Gamma(link="log"))) 
+                        method.args=list(family=Gamma(link="log")))
 mtmodel <- glm(med_transport~training, family=Gamma(link="log"), data=train.data)
 summary(mtmodel) 
 plot(mtmodel)
 # plots look fine ... red lines not as straight as they could be but no major issues
+# normal q-q is a little wonky but I dont think it's an issue ? I think gamma is the correct model for my data
 # looks better than when I incorrectly used the poisson family
 
 # for the inferential stats I will do another model looking at med_transport between groups, instead of over training sessions
@@ -57,7 +59,7 @@ mt1x <- mtx + geom_smooth(method="glm",colour="orange",
                         method.args=list(family=Gamma(link="log"))) 
 mtxmodel <- glm(med_transport~group, family=Gamma(link="log"), data=train.data)
 summary(mtxmodel) 
-plot(mtxmodel) # diagnostic plots look fine
+plot(mtxmodel) # diagnostic plots look fine. Again normal q-q is a bit wonky but not innaproriately so
 library(emmeans)
 mtx.emmeans <- emmeans(mtxmodel,"group")
 plot(mtx.emmeans) # NoChoice group has smaller emmean than Choice group. Makes sense because the nochoice group doesn't have the added time of making decisions
